9a3a57309b62375d6dafdaded9c847842c406775
ebrar- <h_ebrar_@hotmail.com>
2017-03-27 18:08:33 +0300

    Update ortalama.rb

--- ortalama.rb
+++ ortalama.rb
@@ -1,9 +1,9 @@
 #encoding : utf-8
 puts "Ortalamaya Göre Not Hesaplama"
-ortalama=gets
-if (ortalama = 90 and ortalama > 90)
+ortalama = 90
+if (ortalama <=90)
 	puts "A"
-elsif (ortalama < 90 and ortalama > 75)
+elsif (ortalama < 90 and ortalama >= 75)
 	puts "B"
 else 
 	puts "C"
