5f31968815cc78cd0f2b195706a338de5afc5bf1
ebrar- <h_ebrar_@hotmail.com>
2017-03-27 18:02:32 +0300

    Create ortalama.rb

---
+++ ortalama.rb
@@ -0,0 +1,10 @@
+#encoding : utf-8
+puts "Ortalamaya Göre Not Hesaplama"
+ortalama=gets
+if (ortalama = 90 and ortalama > 90)
+	puts "A"
+elsif (ortalama < 90 and ortalama > 75)
+	puts "B"
+else 
+	puts "C"
+puts "#{ortalama}"
